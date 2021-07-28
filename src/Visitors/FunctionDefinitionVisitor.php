504bab5409222a8baca8ab2bed0eb5ac6d89a0a0
Patrick Organ <patrick@permafrost.dev>
2021-07-28 17:22:50 -0400

    add node visitor for function definitions

---
+++ src/Visitors/FunctionDefinitionVisitor.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Permafrost\PhpCodeSearch\Visitors;
+
+use Permafrost\PhpCodeSearch\Results\FileSearchResults;
+use Permafrost\PhpCodeSearch\Results\Nodes\FunctionDefinitionNode;
+use Permafrost\PhpCodeSearch\Support\Arr;
+use PhpParser\Node;
+use PhpParser\NodeVisitorAbstract;
+
+class FunctionDefinitionVisitor extends NodeVisitorAbstract
+{
+    /** @var FileSearchResults */
+    protected $results;
+
+    protected $names = [];
+
+    public function __construct(FileSearchResults $results, array $names)
+    {
+        $this->results = $results;
+        $this->names = $names;
+    }
+
+    public function enterNode(Node $node)
+    {
+        if ($node instanceof Node\Stmt\Function_) {
+            if (Arr::matches($node->name, $this->names, true)) {
+                $resultNode = FunctionDefinitionNode::create($node);
+
+                $this->results->add($resultNode, $resultNode->location());
+            }
+        }
+    }
+}
