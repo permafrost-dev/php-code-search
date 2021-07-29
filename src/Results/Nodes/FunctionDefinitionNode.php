43ade79d79e5b7dc2145f752e592e5ae3932f558
Patrick Organ <patrick@permafrost.dev>
2021-07-29 03:22:23 -0400

    wip

--- src/Results/Nodes/FunctionDefinitionNode.php
+++ src/Results/Nodes/FunctionDefinitionNode.php
@@ -4,17 +4,16 @@ namespace Permafrost\PhpCodeSearch\Results\Nodes;
 
 use Permafrost\PhpCodeSearch\Code\GenericCodeLocation;
 use Permafrost\PhpCodeSearch\Results\Nodes\Traits\HasLocation;
+use Permafrost\PhpCodeSearch\Results\Nodes\Traits\HasName;
 use Permafrost\PhpCodeSearch\Results\Nodes\Traits\TransformsArguments;
 use Permafrost\PhpCodeSearch\Support\StatementTransformer;
 use PhpParser\Node;
 
 class FunctionDefinitionNode implements ResultNode
 {
+    use HasName;
     use HasLocation;
     use TransformsArguments;
-
-    /** @var string */
-    public $name;
 
     /** @var array|ResultNode[]|ValueNode[] */
     public $args;
@@ -22,17 +21,12 @@ class FunctionDefinitionNode implements ResultNode
     public function __construct(Node\Stmt\Function_ $node)
     {
         $this->name = $node->name->toString();
-        $this->args = (new StatementTransformer())->parserNodesToResultNode($node->getParams());
+        $this->args = StatementTransformer::parserNodesToResultNode($node->getParams());
         $this->location = GenericCodeLocation::createFromNode($node);
     }
 
     public static function create(Node\Stmt\Function_ $node): self
     {
         return new static(...func_get_args());
-    }
-
-    public function name(): string
-    {
-        return $this->name;
     }
 }
