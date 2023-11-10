1413b59378faafa085b4cd9fcb403004364a46a6
Patrick Organ <patrick@permafrost.dev>
2023-11-10 15:49:17 -0500

    fix deprecation warnings in php 8.1

--- src/Support/Collections/Enumerable.php
+++ src/Support/Collections/Enumerable.php
@@ -6,16 +6,19 @@ trait Enumerable
 {
     protected $position = 0;
 
+    #[\ReturnTypeWillChange]
     public function offsetExists($offset)
     {
         return isset($this->items[$offset]);
     }
 
+    #[\ReturnTypeWillChange]
     public function offsetGet($offset)
     {
         return $this->items[$offset];
     }
 
+    #[\ReturnTypeWillChange]
     public function offsetSet($key, $value)
     {
         if (is_null($key)) {
@@ -25,31 +28,37 @@ trait Enumerable
         }
     }
 
+    #[\ReturnTypeWillChange]
     public function offsetUnset($offset)
     {
         unset($this->items[$offset]);
     }
 
+    #[\ReturnTypeWillChange]
     public function rewind()
     {
         $this->position = 0;
     }
 
+    #[\ReturnTypeWillChange]
     public function current()
     {
         return $this->items[$this->position];
     }
 
+    #[\ReturnTypeWillChange]
     public function key()
     {
         return $this->position;
     }
 
+    #[\ReturnTypeWillChange]
     public function next()
     {
         ++$this->position;
     }
 
+    #[\ReturnTypeWillChange]
     public function valid()
     {
         return isset($this->items[$this->position]);
